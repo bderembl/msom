91bcf571ffc9cbbb87493e35d4eabea288615df0
bderembl <bruno.deremble@ens.fr>
2019-06-13 15:21:40 +0200

    added backgroud diag

--- msqg/scripts/energy_diag.py
+++ msqg/scripts/energy_diag.py
@@ -6,9 +6,9 @@ import glob,os,re
 import scipy.io.netcdf as netcdf
 
 
-#plt.ion()
+plt.ion()
 
-dir0 = "../outdir_0001/"
+dir0 = "../outdir_0005/"
 exec(open(dir0 + "params.in").read())
 
 filep = 'po*'
@@ -49,6 +49,10 @@ fileFr = 'frpg_' + str(nl) +'l_N' + str(N) + '.bas'
 Fr = np.fromfile(dir0 + fileFr,'f4').reshape(nl,N1,N1).transpose(0,2,1)
 Fr = Fr[:,1:,1:]
 
+fileppg = 'psipg_' + str(nl) +'l_N' + str(N) + '.bas'
+ppg = np.fromfile(dir0 + fileppg,'f4').reshape(nl,N1,N1).transpose(0,2,1)
+ppg = ppg[:,1:,1:]
+
 # create grid
 Delta = L0/N
 
@@ -58,13 +62,25 @@ xc,yc = np.meshgrid(x,x)
 # physical parameters
 Ro = Rom/(1 + Rom*beta*(yc-0.5*L0));
 
-ifi = nb_files - 1
+# backgroud state
+upg = -np.gradient(ppg, axis = 1)/Delta*Ro.reshape(1,N,N)
+vpg = np.gradient(ppg, axis = 2)/Delta*Ro.reshape(1,N,N)
 
-ke_all = np.zeros((nb_files,nl))
-pe_all = np.zeros((nb_files,nl))
+kepg = 0.5*(upg**2 + vpg**2)
 
-for ifi in range(0,nb_files):
-#for ifi in range(0,100):
+bpg = np.diff(ppg,1,0)/dhi.reshape(nl-1,1,1)
+pepg = 0.5*bpg**2*Fr[:-1,:,:]**2
+
+kepg_s = (kepg*dh.reshape(nl,1,1)).sum()*Delta*Delta
+pepg_s = (pepg*dhi.reshape(nl-1,1,1)).sum()*Delta*Delta
+  
+imax = nb_files
+#imax = 5
+
+ke_all = np.zeros((imax))
+pe_all = np.zeros((imax))
+
+for ifi in range(0,imax):
 
   p  = np.fromfile(allfilesp[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
   q  = np.fromfile(allfilesq[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
@@ -74,30 +90,50 @@ for ifi in range(0,nb_files):
   q  = q [:,1:,1:]
   pf = pf[:,1:,1:]
 
-  if len(allfilesbf) > 0:
-    ebf = np.fromfile(allfilesbf[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
-    evd = np.fromfile(allfilesvd[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
-    ej1 = np.fromfile(allfilesj1[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
-    ej2 = np.fromfile(allfilesj2[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
-    ej3 = np.fromfile(allfilesj3[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
-    eft = np.fromfile(allfilesft[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
+  # if len(allfilesbf) > 0:
+  #   ebf = np.fromfile(allfilesbf[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
+  #   evd = np.fromfile(allfilesvd[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
+  #   ej1 = np.fromfile(allfilesj1[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
+  #   ej2 = np.fromfile(allfilesj2[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
+  #   ej3 = np.fromfile(allfilesj3[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
+  #   eft = np.fromfile(allfilesft[ifi],'f4').reshape(nl,N1,N1).transpose(0,2,1)
 
-    ebf = ebf[:,1:,1:]
-    evd = evd[:,1:,1:]
-    ej1 = ej1[:,1:,1:]
-    ej2 = ej2[:,1:,1:]
-    ej3 = ej3[:,1:,1:]
-    eft = eft[:,1:,1:]  
+  #   ebf = ebf[:,1:,1:]
+  #   evd = evd[:,1:,1:]
+  #   ej1 = ej1[:,1:,1:]
+  #   ej2 = ej2[:,1:,1:]
+  #   ej3 = ej3[:,1:,1:]
+  #   eft = eft[:,1:,1:]  
 
+  pt = p
 
-    u = np.gradient(p, axis = 1)/Delta*Ro.reshape(1,N,N)
-    v = np.gradient(p, axis = 2)/Delta*Ro.reshape(1,N,N)
-  
-    ke = 0.5*(u**2 + v**2)
+  u = -np.gradient(pt, axis = 1)/Delta*Ro.reshape(1,N,N)
+  v = np.gradient(pt, axis = 2)/Delta*Ro.reshape(1,N,N)
     
-    b = np.diff(p,1,0)/dhi.reshape(nl-1,1,1)
-    pe = 0.5*b**2*Fr[:-1,:,:]**2
+  ke = 0.5*(u**2 + v**2)
+  
+#  pt = p + ppg
 
-    ke_all[ifi,:] = ke.sum((1,2)) 
-    pe_all[ifi,:-1] = pe.sum((1,2)) 
 
+  b = np.diff(pt,1,0)/dhi.reshape(nl-1,1,1)
+  pe = 0.5*b**2*Fr[:-1,:,:]**2
+  
+  ke_all[ifi] = (ke*dh.reshape(nl,1,1)).sum()*Delta*Delta
+  pe_all[ifi] = (pe*dhi.reshape(nl-1,1,1)).sum()*Delta*Delta
+  
+tt = np.linspace(0,imax-1,imax)*dtout
+
+pe_all = pe_all - pe_all[0]
+
+plt.figure()
+plt.plot(tt,ke_all, label= 'KE')
+plt.plot(tt,pe_all, label= 'PE')
+
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+
+plt.xlabel("time")
+plt.ylabel("E")
+plt.legend()
+
+print("Background PE = {0:.1e}".format(pepg_s))
+print("Background KE = {0:.1e}".format(kepg_s))
