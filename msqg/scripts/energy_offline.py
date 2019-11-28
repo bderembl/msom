4685a00566d3786ec22b73733dbb2e2238be4bf2
bderembl <bruno.deremble@ens.fr>
2019-11-28 17:56:52 +0100

    new diags

---
+++ msqg/scripts/energy_offline.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import glob,os,re,sys
+sys.path.append('../')
+import qg as bas
+import fftlib as myfftlib
+
+
+dir0 = "./"
+dir0 = "../outdir_"
+
+if len(sys.argv) > 1:
+  dir0 = dir0 + str(format(sys.argv[1])).zfill(4) + '/'
+
+# read parameters
+exec(open(dir0 + "params.in").read())
+
+# needed for basilisk
+dirtmp = os.getcwd()
+os.chdir(dir0)
+
+bas.read_params()
+bas.init_grid(N)
+bas.set_vars()
+bas.set_vars_energy()
+bas.set_const()
+
+os.chdir(dirtmp)
+
+p  = np.zeros((nl,N,N))
+q  = np.zeros((nl,N,N))
+bf = np.zeros((nl,N,N))
+vd = np.zeros((nl,N,N))
+j1 = np.zeros((nl,N,N))
+j2 = np.zeros((nl,N,N))
+j3 = np.zeros((nl,N,N))
+ft = np.zeros((nl,N,N))
+
+filep = 'po*'
+fileq = 'qo*'
+filef = 'pf*'
+
+filebf = 'de_bf*'
+filevd = 'de_vd*'
+filej1 = 'de_j1*'
+filej2 = 'de_j2*'
+filej3 = 'de_j3*'
+fileft = 'de_ft*'
+
+allfilesp = sorted(glob.glob(dir0 + filep));
+allfilesq = sorted(glob.glob(dir0 + fileq));
+allfilesf = sorted(glob.glob(dir0 + filef));
+nb_files  = len(allfilesp);
+
+allfilesbf = sorted(glob.glob(dir0 + filebf));
+allfilesvd = sorted(glob.glob(dir0 + filevd));
+allfilesj1 = sorted(glob.glob(dir0 + filej1));
+allfilesj2 = sorted(glob.glob(dir0 + filej2));
+allfilesj3 = sorted(glob.glob(dir0 + filej3));
+allfilesft = sorted(glob.glob(dir0 + fileft));
+
+b = np.fromfile(allfilesp[0],'f4')
+N = int(b[0])
+N1 = N + 1
+nl = int(len(b)/N1**2)
+
+fileh = 'dh_' + str(nl) +'l.bin'
+
+dh = np.fromfile(dir0 + fileh,'f4')
+dhi = 0.5*(dh[:-1] + dh[1:])
+
+fileFr = 'frpg_' + str(nl) +'l_N' + str(N) + '.bas'
+Fr = np.fromfile(dir0 + fileFr,'f4').reshape(nl,N1,N1).transpose(0,2,1)
+Fr = Fr[:,1:,1:]
+
+# grid
+Delta = L0/N
+x = np.linspace(0.5*Delta, L0-0.5*Delta,N)
+y = np.linspace(0.5*Delta, L0-0.5*Delta,N)
+xc, yc = np.meshgrid(x,y)
+
+if (Rom < 0) :
+  Ro = 0*yc - Rom
+else:
+  Ro = Rom/(1 + Rom*beta*(yc-0.5*L0))
+
+
+Nkr = myfftlib.get_len_wavenumber(N)
+
+eflux_bf = np.zeros(Nkr)
+eflux_vd = np.zeros(Nkr)
+eflux_j1 = np.zeros(Nkr)
+eflux_j2 = np.zeros(Nkr)
+eflux_j3 = np.zeros(Nkr)
+eflux_ft = np.zeros(Nkr)
+
+nme = 0
+
+nt0 = -2
+nt1 = -1
+for nt in range (nt0,nt1):
+
+  p  = np.fromfile(allfilesp[nt],'f4').reshape(nl,N1,N1).transpose(0,2,1)
+  q  = np.fromfile(allfilesq[nt],'f4').reshape(nl,N1,N1).transpose(0,2,1)
+  p  = p[:,1:,1:]
+  q  = q[:,1:,1:]
+
+  bas.pystep(q,bf,vd,j1,j2,j3,ft)
+  nme += 1
+
+  for il in range(0,nl):
+    kspec,flux_bf  = myfftlib.get_flux(-p[il,:,:],bf[il,:,:],Delta)
+    kspec,flux_vd  = myfftlib.get_flux(-p[il,:,:],vd[il,:,:],Delta)
+    kspec,flux_j1  = myfftlib.get_flux(-p[il,:,:],j1[il,:,:],Delta)
+    kspec,flux_j2  = myfftlib.get_flux(-p[il,:,:],j2[il,:,:],Delta)
+    kspec,flux_j3  = myfftlib.get_flux(-p[il,:,:],j3[il,:,:],Delta)
+    kspec,flux_ft  = myfftlib.get_flux(-p[il,:,:],ft[il,:,:],Delta)
+    
+    eflux_bf +=  flux_bf*dh[il]
+    eflux_vd +=  flux_vd*dh[il]
+    eflux_j1 +=  flux_j1*dh[il]
+    eflux_j2 +=  flux_j2*dh[il]
+    eflux_j3 +=  flux_j3*dh[il]
+    eflux_ft +=  flux_ft*dh[il]
+
+eflux_bf /= nme
+eflux_vd /= nme
+eflux_j1 /= nme
+eflux_j2 /= nme
+eflux_j3 /= nme
+eflux_ft /= nme
+
+plt.figure()
+plt.plot(np.log10(kspec), eflux_bf, label= 'bf')
+plt.plot(np.log10(kspec), eflux_vd, label= 'vd')
+plt.plot(np.log10(kspec), eflux_j1, label= 'j1')
+plt.plot(np.log10(kspec), eflux_j2, label= 'j2')
+plt.plot(np.log10(kspec), eflux_j3, label= 'j3')
+plt.plot(np.log10(kspec), eflux_ft, label= 'ft')
+
+plt.legend()
+
+
+bas.trash_vars()
+bas.trash_vars_energy()
---
+++ msqg/scripts/fftlib.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+
+def radial_average(spec_2D,Delta):
+  ''' Compute the azimuthal avearge of the 2D spectrum '''
+
+  KK,kr = get_wavenumber(spec_2D,Delta)
+  spec_1D = np.zeros(len(kr))
+  for i in range(kr.size):
+    kfilt =  (KK>=kr[i] - kr[0]) & (KK<=kr[i])
+    Nbin = kfilt.sum()
+    spec_1D[i] = (spec_2D[kfilt].sum())*kr[i]/Nbin
+  return kr,spec_1D
+
+
+def get_len_wavenumber(N):
+  kx = np.fft.fftshift(np.fft.fftfreq(N))
+  dk = np.abs(kx[2]-kx[1])
+  return (int(kx.max()/dk)-1)
+
+
+def get_wavenumber(spec_2D,Delta):
+  ''' Compute wavenumber and radial wavenumber '''
+  N,N2 = np.shape(spec_2D) # assume N = N2
+  kx = np.fft.fftshift(np.fft.fftfreq(N,Delta)) # two sided  
+  k, l = np.meshgrid(kx,kx)
+  KK = np.sqrt(k**2 + l**2)  
+  dk = np.abs(kx[2]-kx[1])
+  kr = dk*np.arange(1,int(k.max()/dk))
+  return KK,kr
+
+
+def get_spec_2D(psi1,psi2,Delta):
+  ''' Compute the 2D power spectrum of the data 
+
+   normalization such that parseval is ok: 
+   E = np.sum(psi**2)*Delta**2 
+     = np.sum(psi_hat*psi_hat.conj()).real*dk**2 
+     ~ np.sum(spec_1D)*dk*2*np.pi '''
+  
+  psi1_hat = np.fft.fft2(psi1)*Delta**2
+  psi2_hat = np.fft.fft2(psi2)*Delta**2
+  spec_2D = (psi1_hat*psi2_hat.conj()).real
+  spec_2D = np.fft.fftshift(spec_2D)
+  return spec_2D
+
+
+def get_spec_1D(psi1,psi2,Delta):
+  ''' Compute the 2D power spectrum of the data '''
+
+  spec_2D = get_spec_2D(psi1,psi2,Delta)
+  kr,spec_1D = radial_average(spec_2D,Delta)
+  return kr,spec_1D
+
+
+def get_flux(psi1,psi2,Delta):
+  ''' Compute flux'''
+  spec_2D = get_spec_2D(psi1,psi2,Delta)
+
+  # kr,spec_1D = radial_average(spec_2D,Delta)
+  # flux = -np.cumsum(spec_1D)*dk # integrate from low wavenumbers
+  # flux = np.cumsum(spec_1D[::-1])[::-1]*dk # integrate from high wavenumbers
+
+  KK,kr = get_wavenumber(spec_2D,Delta)
+  dk = kr[1] - kr[0]
+
+  flux = np.zeros(len(kr))
+  for i in range(kr.size):
+    kfilt =  (kr[i] <= KK ) 
+    flux[i] = (spec_2D[kfilt]).sum()*dk*dk
+  return kr,flux
+  
